5197cbcc719d5a84ed71bb9aca5d7b33ae1b5125
nicelhc13 <j_s1195@txstate.edu>
2019-10-03 10:46:15 -0700

    added missing poison

--- IdentifyPoison.cpp
+++ IdentifyPoison.cpp
@@ -15,20 +15,22 @@ std::string conclusion(int diagnosis) {
 
   poison.insert({3, "ricin"});
   poison.insert({4, "anthrax"});
-  poison.insert({11, "chlordecone"});
-  poison.insert({12, "stychinine"});
+  poison.insert({9, "chlordecone"});
+  poison.insert({10, "carbon monoxide"});
+  poison.insert({11, "fentanyl"});
+  poison.insert({12, "strychinine"});
   poison.insert({14, "amatoxin"});
   poison.insert({17, "rotenone"});
-  poison.insert({18, "sodium fluoracetate"});
+  poison.insert({18, "sodium fluoroacetate"});
   poison.insert({19, "dioxin"});
   poison.insert({21, "tetrodotoxin"});
   poison.insert({22, "nitroprusside"});
   poison.insert({23, "batrachotoxin"});
   poison.insert({30, "methanol"});
   poison.insert({31, "iodine"});
-  poison.insert({32, "ampetamine"});
+  poison.insert({32, "amphetamine"});
   poison.insert({33, "chlorine"});
-  poison.insert({34, "meethamphetamine"});
+  poison.insert({34, "methamphetamine"});
   poison.insert({35, "formaldehyde"});
   poison.insert({36, "ibuprofen"});
   poison.insert({37, "isopropanol"});
@@ -75,7 +77,7 @@ std::string BuildKnowledgeBase(std::vector<std::string> symptoms) {
   symptomVar["vomiting"] = 4;
   symptomVar["nausea"] = 5;
   symptomVar["confusion"] = 6;
-  symptomVar["diziness"] = 7;
+  symptomVar["dizziness"] = 7;
   symptomVar["muscle contraction"] = 8;
   symptomVar["abdominal pain"] = 9;
   symptomVar["diarrhea"] = 10;
@@ -84,7 +86,7 @@ std::string BuildKnowledgeBase(std::vector<std::string> symptoms) {
   symptomVar["pharyngeal swelling"] = 13;
   symptomVar["seizure"] = 14;
   symptomVar["throat pain"] = 15;
-  symptomVar["parathesia"] = 16;
+  symptomVar["paresthesia"] = 16;
   symptomVar["irritation of skin"] = 17;
   symptomVar["breathing problems"] = 18;
   symptomVar["coma"] = 19;
@@ -92,7 +94,7 @@ std::string BuildKnowledgeBase(std::vector<std::string> symptoms) {
   symptomVar["paranoia"] = 21;
   symptomVar["renal impairment"] = 22;
   symptomVar["heart failure"] = 23;
-  symptomVar["mudriasis"] = 24;
+  symptomVar["mydriasis"] = 24;
   symptomVar["excessive salivation"] = 25;
   symptomVar["muscle weakness"] = 26; 
   symptomVar["drowsiness"] = 27;
